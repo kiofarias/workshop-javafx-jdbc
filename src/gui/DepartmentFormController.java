33a1eb9ddc56723e794e66c523840635ddd95904
Francisco Farias <kiofarias@gmail.com>
2019-02-17 10:00:55 -0300

    Criação do padrão observer para atualização das tabelas

--- src/gui/DepartmentFormController.java
+++ src/gui/DepartmentFormController.java
@@ -1,19 +1,22 @@
 package gui;
 
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
 
 import db.DbException;
+import gui.listeners.DataChangeListener;
 import gui.util.Alerts;
 import gui.util.Constraints;
 import gui.util.Utils;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
-import javafx.scene.control.Alert.AlertType;
 import model.entities.Department;
 import model.services.DepartmentService;
 
@@ -34,12 +37,18 @@ public class DepartmentFormController implements Initializable {
 	
 	private DepartmentService service;
 	
+	private List<DataChangeListener>  dataChangeListeners = new ArrayList<>();
+	
 	public void setDepartment(Department entity) {
 		this.entity=entity;
 	}
 	
 	public void setDepartmentService(DepartmentService service) {
 		this.service=service;
+	}
+	
+	public void subscribeDataChangeListener(DataChangeListener listener) {
+		dataChangeListeners.add(listener);
 	}
 	
 	@FXML
@@ -53,13 +62,19 @@ public class DepartmentFormController implements Initializable {
 		try {
 			entity = getFormData();
 			service.saveOrUpdate(entity);
-			Utils.currentStage(event).close();;
+			notifyDataChangeListeners();
+			Utils.currentStage(event).close();
 		} catch (DbException e) {
 			Alerts.showAlert("Error saving object", null, e.getMessage(), AlertType.ERROR);
 		}
 		
 	}
 	
+	private void notifyDataChangeListeners() {
+
+		dataChangeListeners.forEach(DataChangeListener::onDataChanged);
+	}
+
 	private Department getFormData() {
 		Department obj = new Department();
 		obj.setId(Utils.tryParsetoInt(txtId.getText()));
