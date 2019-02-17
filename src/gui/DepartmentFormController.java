3a900a4297b72008d3788ff27e52e3a64a6ca164
Francisco Farias <kiofarias@gmail.com>
2019-02-17 08:25:32 -0300

    Passando Department para a view DepartmentForm

--- src/gui/DepartmentFormController.java
+++ src/gui/DepartmentFormController.java
@@ -9,6 +9,7 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import model.entities.Department;
 
 public class DepartmentFormController implements Initializable {
 
@@ -22,6 +23,12 @@ public class DepartmentFormController implements Initializable {
 	private Button btSave;
 	@FXML
 	private Button btCancel;
+	
+	private Department entity;
+	
+	public void setDepartment(Department entity) {
+		this.entity=entity;
+	}
 	
 	@FXML
 	public void onBtSaveAction() {
@@ -43,5 +50,13 @@ public class DepartmentFormController implements Initializable {
 		Constraints.setTextFieldIntegert(txtId);
 		Constraints.setTextFieldMaxLenght(txtName,30);
 	}
-
+	
+	public void updateFormData() {
+		if(entity==null) {
+			throw new IllegalStateException("Entity was null");
+		}
+		txtId.setText(String.valueOf(entity.getId()));
+		txtName.setText(entity.getName());
+	}
+	
 }
